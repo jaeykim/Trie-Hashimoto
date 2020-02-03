8c336e273123dc0f2a0beae0e8c931afaa97c9e3
jmlee <jmlee@altair.snu.ac.kr>
2020-02-03 12:59:37 +0900

    increase hasher buffer length & append nonce to branch node encoding

--- trie/hasher.go
+++ trie/hasher.go
@@ -17,8 +17,10 @@
 package trie
 
 import (
+	_ "fmt"
 	"hash"
 	"sync"
+	"encoding/binary"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/rlp"
@@ -54,7 +56,8 @@ func (b *sliceBuffer) Reset() {
 var hasherPool = sync.Pool{
 	New: func() interface{} {
 		return &hasher{
-			tmp: make(sliceBuffer, 0, 550), // cap is as large as a full fullNode.
+			// tmp: make(sliceBuffer, 0, 550), // cap is as large as a full fullNode.
+			tmp: make(sliceBuffer, 0, 600), // cap is as large as a full fullNode. (increase buf size to append nonce as a byte array) (jmlee)
 			sha: sha3.NewLegacyKeccak256().(keccakState),
 		}
 	},
@@ -73,16 +76,23 @@ func returnHasherToPool(h *hasher) {
 // hash collapses a node down into a hash node, also returning a copy of the
 // original node initialized with the computed hash to replace the original one.
 func (h *hasher) hash(n node, db *Database, force bool) (node, node, error) {
+	// fmt.Println("hasher.hash() called")
+	// hn, b := n.cache()
+	// fmt.Println("	node info before start hasher.hash() -> ", "/ node info -> hash:", hn.fstring(""), ", dirty:", b, ", nonce:", n.getNonce())
 	// If we're not storing the node, just hashing, use available cached data
 	if hash, dirty := n.cache(); hash != nil {
 		if db == nil {
+			// fmt.Println("hasher.hash() finished 1", "/ hash:", hash.fstring(""), "/ dirty:", dirty)
 			return hash, n, nil
 		}
 		if !dirty {
 			switch n.(type) {
 			case *fullNode, *shortNode:
+				// hn, b := n.cache()
+				// fmt.Println("hasher.hash() finished 2", "/ node info -> hash:", hn.fstring(""), ", dirty:", b, ", nonce:", n.getNonce())
 				return hash, hash, nil
 			default:
+				// fmt.Println("hasher.hash() finished 3")
 				return hash, n, nil
 			}
 		}
@@ -90,10 +100,12 @@ func (h *hasher) hash(n node, db *Database, force bool) (node, node, error) {
 	// Trie not processed yet or needs storage, walk the children
 	collapsed, cached, err := h.hashChildren(n, db)
 	if err != nil {
+		// fmt.Println("hasher.hash() finished 4")
 		return hashNode{}, n, err
 	}
 	hashed, err := h.store(collapsed, db, force)
 	if err != nil {
+		// fmt.Println("hasher.hash() finished 5")
 		return hashNode{}, n, err
 	}
 	// Cache the hash of the node for later reuse and remove
@@ -112,6 +124,8 @@ func (h *hasher) hash(n node, db *Database, force bool) (node, node, error) {
 			cn.flags.dirty = false
 		}
 	}
+	// hn, b = cached.cache()
+	// fmt.Println("hasher.hash() finished 6", "/ node info -> hash:", hn.fstring(""), ", dirty:", b, ", nonce:", cached.getNonce())
 	return hashed, cached, nil
 }
 
@@ -175,6 +189,44 @@ func (h *hasher) store(n node, db *Database, force bool) (node, error) {
 	}
 	// Larger nodes are replaced by their hash and stored in the database.
 	hash, _ := n.cache()
+	switch n.(type) {
+		case *fullNode:
+			// 강제로 nonce 값이 들어가도록 만들었음 
+			// TODO: bytesNonce 에서 0이 아닌 애들만 붙여줘도 괜찮을듯 (작은 최적화)
+			byteNonce := make([]byte, binary.MaxVarintLen64)
+			binary.PutUvarint(byteNonce, n.getNonce())
+			h.tmp = append(h.tmp, byteNonce...)
+			// fmt.Println("appended byte:", h.tmp)
+	}
+
+	// if n.getNonce() != 0 {
+	// 	fmt.Println("INFOOO")
+	// 	byteNonce := make([]byte, binary.MaxVarintLen64)
+	// 	binary.PutUvarint(byteNonce, n.getNonce())
+	// 	fmt.Println("n.nonce:", n.getNonce(), "/ bytes:", byteNonce)
+	// 	fmt.Println("n.cache.hash:", common.BytesToHash(hash).Hex())
+	// 	fmt.Println("h.tmp:", h.tmp)
+	
+	// 	// 예상이 맞았음
+	// 	// n이 branch node이면 nonce가 바뀌어도 h.tmp 가 바뀌지 않음
+
+	// 	switch nn := n.(type) {
+	// 		case *fullNode:
+	// 			fmt.Println("print branch node!!!")
+	// 			for i := 0; i < 16; i++ {
+	// 				if nn.Children[i] != nil {
+	// 					fmt.Println("at branch", i, ": ", nn.Children[i])
+	// 				}
+	// 			}
+	// 			// 강제로 nonce 값이 들어가도록 만들었음 
+	// 			// TODO: bytesNonce 에서 0이 아닌 애들만 붙여줘도 괜찮을듯 (작은 최적화)
+	// 			h.tmp = append(h.tmp, byteNonce...)
+	// 			fmt.Println("appended byte:", h.tmp)
+	// 		// default:
+	// 		// 	fmt.Println("HEHEHE")
+	// 	}
+	// }
+	// fmt.Println("h.tmp:", h.tmp)
 	if hash == nil {
 		hash = h.makeHashNode(h.tmp)
 	}
