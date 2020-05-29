d468080224fabef05f0bc41423fd9bd375423ddf
jmlee <jmlee@altair.snu.ac.kr>
2020-05-29 17:14:23 +0900

    add comments about Get() & print FINE THE VALUE AT LEVEL x

--- vendor/github.com/syndtr/goleveldb/leveldb/version.go
+++ vendor/github.com/syndtr/goleveldb/leveldb/version.go
@@ -104,6 +104,7 @@ func (v *version) walkOverlapping(aux tFiles, ikey internalKey, f func(level int
 	ukey := ikey.ukey()
 
 	// Aux level.
+	// aux = nil로 들어오니까 무시하자
 	if aux != nil {
 		for i, t := range aux {
 			// fmt.Println("Aux: " + strconv.Itoa(i))
@@ -144,6 +145,7 @@ func (v *version) walkOverlapping(aux tFiles, ikey internalKey, f func(level int
 			for _, t := range tables {
 				if t.overlaps(v.s.icmp, ukey, ukey) {
 					if !f(level, t) {
+						// f 함수의 결과가 false면 탐색을 끝냄
 						return
 					}
 				}
@@ -160,16 +162,22 @@ func (v *version) walkOverlapping(aux tFiles, ikey internalKey, f func(level int
 		}
 
 		if lf != nil && !lf(level) {
+			// level0에서 원하던 값을 찾았었으면 이제 찾은 것으로 확정 짓고 끝냄
 			return
 		}
+		// 못찾았으면 다음 level 탐색하러 감
 	}
 }
 
+// db.Get()에서 값을 찾으려 하는데 memory에서 못찾으면 disk에서 찾기 위해 이 함수를 부르게 됨
+// aux = nil, ro = nil, noValue = false를 넣어 호출
 func (v *version) get(aux tFiles, ikey internalKey, ro *opt.ReadOptions, noValue bool) (value []byte, tcomp bool, err error) {
+	// 만약 version이 닫혔으면? 닫히는 중이면? 아무튼 못찾았다고 하나봄, 넘어가고
 	if v.closing {
 		return nil, false, ErrClosed
 	}
 
+	// 찾고자하는 key를 db.Get()에서 internalKey로 바꾸고 그걸 또 ukey라는 형태로 바꿈
 	ukey := ikey.ukey()
 	sampleSeeks := !v.s.o.GetDisableSeeksCompaction()
 
@@ -188,7 +196,11 @@ func (v *version) get(aux tFiles, ikey internalKey, ro *opt.ReadOptions, noValue
 
 	// Since entries never hop across level, finding key/value
 	// in smaller level make later levels irrelevant.
+	// aux = nil로 들어오니까 무시하자
 	v.walkOverlapping(aux, ikey, func(level int, t *tFile) bool {
+		// 이게 walkOverlapping 함수에서 f 함수인데 뭐하는 함수냐면
+		// 해당 level에 있는 table 하나에서 찾고자 하는 값을 서치하는 코드임
+		// 이 함수를 모든 레벨에 있는 모든 table 마다 실행시키는 식으로 iterate 하면서 찾는거임
 		if sampleSeeks && level >= 0 && !tseek {
 			if tset == nil {
 				tset = &tSet{level, t}
@@ -204,22 +216,45 @@ func (v *version) get(aux tFiles, ikey internalKey, ro *opt.ReadOptions, noValue
 		if noValue {
 			fikey, ferr = v.s.tops.findKey(t, ikey, ro)
 		} else {
+			// noValue = false 라서 이게 실행됨
+			// tops는 tOps 타입인데 table operations 를 뜻함
+			// tops.find() 함수 설명 보면 이렇게 나옴
+			// Finds key/value pair whose key is greater than or equal to the given key.
 			fikey, fval, ferr = v.s.tops.find(t, ikey, ro)
 		}
 
 		switch ferr {
 		case nil:
+			// v.s.tops.find() 함수에서 해당 ikey보다 더 크거나 같은 key/value pair를 return 한다는데
+			// 이게 ferr 안일어나고 성공했으면 이거 가지고서 밑에서 한번 찾아보는듯
+			// table에서 찾으려는 ikey 보다 더 큰값이 있다는건 해당 range 안에 들어있다는 거니까
+			// 밑에서 한번 찾아보려고 하나봄
 		case ErrNotFound:
+			// walkOverlapping 함수에서 보면 f가 false 면 함수를 종료하던데
+			// 이렇게 못찾았다고 ErrNotFound가 나왔을 때 true를 return 하니까
+			// 여긴 없구나, 다른 곳으로 찾으러가자 이런 의미로 true를 return 시키는거 같음
 			return true
 		default:
+			// 문제가 있으면 f를 false로 만들어서
+			// walkOverlapping 함수를 종료시켜버림
+			// 에러가 났으니까 찾고 자시고 간에 그냥 문제 있다고 종료시키는거 같음
 			err = ferr
 			return false
 		}
 
+		// 자 위에서 v.s.tops.find()에서 가져온 정보를 바탕으로 한번 보자
+		// 
 		if fukey, fseq, fkt, fkerr := parseInternalKey(fikey); fkerr == nil {
+			// ukey가 내가 찾고자 하는 key값이고
+			// fukey가 find()함수를 통해 가져온 값인데 이걸 비교해보고 같은지 아닌지를 보는듯
+			// 둘이 같다면 원하던 값을 찾아냈다는 거지
+			// (ukey <= fukey 니까)
 			if v.s.icmp.uCompare(ukey, fukey) == 0 {
 				// Level <= 0 may overlaps each-other.
 				if level <= 0 {
+					// 찾아낸게 level0에서라면 잠시 저장해두고서 일단 넘어가는듯
+					// level0에서 찾은 경우에는 다 sort 되어 있는게 아니고 뭔가 overlapping 이 있을 수 있기에
+					// 이런 식으로 하는건가? 
 					if fseq >= zseq {
 						zfound = true
 						zseq = fseq
@@ -227,36 +262,49 @@ func (v *version) get(aux tFiles, ikey internalKey, ro *opt.ReadOptions, noValue
 						zval = fval
 					}
 				} else {
+					// level0에서 찾았을 때와는 다르게
+					// level1 이상에서 원하던 값을 찾았다면 찾은 것으로 확정지어버리는 듯
 					switch fkt {
 					case keyTypeVal:
+						fmt.Println("@@FIND THE VALUE!: at level", level) // 여긴 level1 이상이 나옴
 						value = fval
 						err = nil
 					case keyTypeDel:
 					default:
 						panic("leveldb: invalid internalKey type")
 					}
+					// 찾았던 오류가 났던간에 아무튼 walkOverlapping 함수를 종료시켜 탐색 끝냄
 					return false
 				}
 			}
 		} else {
+			// parseInternalKey 함수가 에러가 났으면 fikey가 뭔가 이상한거니까
+			// 그냥 false 를 return 해서 walkOverlapping 함수를 종료시켜 탐색 끝냄
 			err = fkerr
 			return false
 		}
 
+		// fukey가 찾고자 하던 ukey 보다 큰 경우 / level0에서 찾던 값이 있엇다면 
+		// walkOverlapping를 통한 탐색을 계속 진행시킴
 		return true
+
 	}, func(level int) bool {
 		if zfound {
 			switch zkt {
 			case keyTypeVal:
+				fmt.Println("@FIND THE VALUE!: at level", level) // 여긴 level0가 나옴
 				value = zval
 				err = nil
 			case keyTypeDel:
 			default:
 				panic("leveldb: invalid internalKey type")
 			}
+			// 원하는 값을 찾았던 / 뭔가의 에러가 있어 못찾았던 간에 
+			// return false 를 통해 walkOverlapping 함수를 종료시켜버림
 			return false
 		}
 
+		// level0에서 원하던 값을 찾은게 아니라면 true를 return해 walkOverlapping 함수를 통해 계속 탐색시킴
 		return true
 	})
 
