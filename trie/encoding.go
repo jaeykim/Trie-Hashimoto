955e27e393fc77f64a9b306df03945a7309cc0bd
jmlee <jmlee@altair.snu.ac.kr>
2020-06-26 15:58:19 +0900

    delete duplicated function

--- trie/encoding.go
+++ trie/encoding.go
@@ -74,40 +74,6 @@ func hexToHashPrefix (hex []byte) []byte {
 	return buf
 }
 
-// compactToHashPrefix returns the hash prefix we want to find (sjkim)
-func compactToHashPrefix (compact []byte) []byte {
-	//return nil // do not mining (sjkim)
-	fixedLength := 2
-	terminator := byte(0)
-	if compact[0] >> 5 == 1 {
-		terminator = byte(1)
-		//compact = compact[:len(compact)-1] // fast mining for leaf node 
-	}
-	if compact[0] & byte(0x10) == 0x10 {
-		length := len(compact)
-		if length > 4 { length = 4; }
-		if len(compact) > fixedLength { compact = compact[:fixedLength]; }
-		buf := make([]byte, fixedLength)
-		copy(buf, compact)
-		buf[0] &= byte(0x0f)
-		buf[0] |= byte(2*length-1) << 4
-		buf[0] |= terminator << 7
-		return buf
-	} else if compact[0] & byte(0x10) == 0x00 {
-		length := len(compact)
-		if length > 4 { length = 4; }
-		if len(compact) > fixedLength { compact = compact[:fixedLength]; }
-		buf := make([]byte, fixedLength)
-		copy(buf, compact)
-		buf[0] &= byte(0x0f)
-		buf[0] |= byte(2*length-2) << 4
-		buf[0] |= terminator << 7
-		return buf
-	} else {
-		panic("compactToHashPresix error")
-	}
-}
-
 func compactToHex(compact []byte) []byte {
 	if len(compact) == 0 {
 		return compact
