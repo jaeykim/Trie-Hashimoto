f7610a84a9714b569b68f400de7c196b5dab6235
sms7002 <sms7002@gmail.com>
2020-07-27 17:18:17 +0900

    removed useless flag

--- trie/node.go
+++ trie/node.go
@@ -22,7 +22,7 @@ import (
 	"strings"
 	"math/big"
 	"reflect"
-	"encoding/binary" // (sjkim)
+	"encoding/binary"
 	
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/rlp"
