21a530ab8401dfbd749ea1c6af10bb63f34585a9
jmlee <jmlee@altair.snu.ac.kr>
2020-08-04 17:47:09 +0900

    add getCompactionInfo.py functionalities

--- build/bin/experiment/analyzeLogFiles.py
+++ build/bin/experiment/analyzeLogFiles.py
@@ -15,9 +15,12 @@ ORIGINALCSVPATH = ORIGINALLOGPATH + 'csv/'
 ORIGINALGRAPHPATH = ORIGINALLOGPATH + 'graph/'
 
 # make empty 2d list -> list[b][a]
-def TwoD(a, b):
-    return np.zeros(a * b, dtype=int).reshape(b, a)
-
+def TwoD(a, b, isInt):
+    if isInt:
+        return np.zeros(a * b, dtype=int).reshape(b, a)
+    else:
+        return np.zeros(a * b, dtype=float).reshape(b, a)
+    
 # make the directory if not exist
 def makeDir(path):
     Path(path).mkdir(parents=True, exist_ok=True) 
@@ -47,7 +50,7 @@ def analyzeBlockProcessTimeLog(isIMPT):
     # times[4][blockNum] = stateFlushTime (ns)
     # times[5][blockNum] = dirtyNodesCount
     # times[6][blockNum] = blockInsertedTime(Unix time with ns)
-    times = TwoD(MAXBLOCKNUM+1, TIMESNUM)
+    times = TwoD(MAXBLOCKNUM+1, TIMESNUM, True)
 
     # read data from log file
     f = open(LOGFILEPATH, 'r')
@@ -163,7 +166,7 @@ def analyzeDatabaseInspectLog(isIMPT):
     makeDir(GRAPHPATH)
 
     LINENUM = sum(1 for line in open(LOGFILEPATH))
-    sizes = TwoD(LINENUM, SIZESNUM) # sizes[contents index] = list of its inspected sizes
+    sizes = TwoD(LINENUM, SIZESNUM, False) # sizes[contents index] = list of its inspected sizes
 
     # read data from log file
     f = open(LOGFILEPATH, 'r')
@@ -234,7 +237,7 @@ def analyzeDatabaseReadTimeLog(isIMPT):
     # logDatas[0][blockNum] = total database read time (ns)
     # logDatas[1][blockNum] = # of database read
     # logDatas[2][blockNum] = database size (KB)
-    logDatas = TwoD(MAXBLOCKNUM+1, DATANUM)
+    logDatas = TwoD(MAXBLOCKNUM+1, DATANUM, True)
 
     # read data from log file
     f = open(LOGFILEPATH, 'r')
@@ -277,6 +280,208 @@ def analyzeDatabaseReadTimeLog(isIMPT):
         wr.writerows(export_data)
     myfile.close()
 
+    #
+    # TODO?: drawing graphs
+    #
+
+
+# analyze impt_leveldb_compaction_log.txt log files
+def analyzeLevelDBCompaction(isIMPT):
+
+    MAXBLOCKNUM = 1340
+    DBNUM = 1   # num of db in the log file (1 means no additional db for trie nodes)
+    MAXLEVEL = 7   # max num of levels in levelDB (ex. 7 means level 0~6 exists)
+    COMPACTIONTYPESNUM = 5  # memory, level0, non-level0, seek, move compaction
+
+    if isIMPT:
+        LOGFILEPATH = IMPTLOGPATH + 'impt_leveldb_compaction_log.txt'
+        GRAPHPATH = IMPTGRAPHPATH + 'leveldbCompaction/'
+    else:
+        LOGFILEPATH = ORIGINALLOGPATH + 'impt_leveldb_compaction_log.txt'
+        GRAPHPATH = ORIGINALGRAPHPATH + 'leveldbCompaction/'
+    makeDir(GRAPHPATH)
+
+    # compactions[0][blockNum] = # of memory compaction
+    # compactions[1][blockNum] = # of level0 compaction
+    # compactions[2][blockNum] = # of non-level0 compaction
+    # compactions[3][blockNum] = # of seek compaction
+    # compactions[4][blockNum] = # of move compaction
+    compactions = TwoD(MAXBLOCKNUM+1, COMPACTIONTYPESNUM, True)
+
+    # tableCounts[level][blockNum] = # of tables at the level
+    tableCounts = TwoD(MAXBLOCKNUM+1, MAXLEVEL, True)
+
+    # tableSizes[level][blockNum] = table size at the level (MB)
+    tableSizes = TwoD(MAXBLOCKNUM+1, MAXLEVEL, False)
+
+    # durations[level][blockNum] = duration time at the level (sec)
+    durations = TwoD(MAXBLOCKNUM+1, MAXLEVEL, False)
+
+    # readSizes[level][blockNum] = read size at the level (MB)
+    readSizes = TwoD(MAXBLOCKNUM+1, MAXLEVEL, False)
+
+    # writeSizes[level][blockNum] = write size at the level (MB)
+    writeSizes = TwoD(MAXBLOCKNUM+1, MAXLEVEL, False)
+
+    # read data from log file
+    f = open(LOGFILEPATH, 'r')
+    rdr = csv.reader(f)
+    blockNum = 1
+    for line in rdr:
+        if len(line) == 0:
+            continue
+        
+        if len(line) > 1:
+            if len(line) == 6:
+                # find compaction count log
+                # print("find compaction count info")
+                # memComp = int(line[0])
+                # level0Comp = int(line[1])
+                # nonLevel0Comp = int(line[2])
+                # seekComp = int(line[3])
+                # moveComp = int(line[4])
+                compactions[0][blockNum] = int(line[0])
+                compactions[1][blockNum] = int(line[1])
+                compactions[2][blockNum] = int(line[2])
+                compactions[3][blockNum] = int(line[3])
+                compactions[4][blockNum] = int(line[4])
+
+            else:
+                # find leveldb info log
+                # print("find leveldb info log")
+                level = int(line[0])
+                # tables = int(line[1])
+                # tablesSize = float(line[2])    # MB
+                # duration = float(line[3])      # sec
+                # readSize = float(line[4])      # MB
+                # writeSize = float(line[5])     # MB
+
+                tableCounts[level][blockNum] = int(line[1])
+                tableSizes[level][blockNum] = float(line[2])
+                durations[level][blockNum] = float(line[3])
+                readSizes[level][blockNum] = float(line[4])
+                writeSizes[level][blockNum] = float(line[5])
+
+        else:
+            if line[0] == "leveldbInfo":
+                # find "leveldbInfo"
+                # print("find leveldbinfo")
+                pass
+            else:
+                # find "inserted block x --------------------"
+                # x = int(line[0].split(" ")[2])
+                print("now finished block", blockNum)
+                if blockNum == MAXBLOCKNUM:
+                    break
+                blockNum = blockNum + 1
+
+    f.close()
+
+
+
+    # make log data as a csv file
+    blockNums = list(range(1,MAXBLOCKNUM+1))
+    data = [blockNums, compactions[0][1:], compactions[1][1:], compactions[2][1:], compactions[3][1:], compactions[4][1:]]
+    export_data = zip_longest(*data, fillvalue = '')
+    if isIMPT:
+        csvFilePath = IMPTCSVPATH
+    else:
+        csvFilePath = ORIGINALCSVPATH
+    with open(csvFilePath + "leveldb_compaction_count.csv", 'w', encoding="ISO-8859-1", newline='') as myfile:
+        wr = csv.writer(myfile)
+        wr.writerow(("block number", "memory compaction", "level0 compaction", "non-level0 compaction", "seek compaction", "move compaction"))
+        wr.writerows(export_data)
+    myfile.close()
+
+    data = [blockNums]
+    columnTitles = ["block number"]
+    for i in range(MAXLEVEL):
+        data.append(tableCounts[i][1:])
+        columnTitles.append("level " + str(i))
+    export_data = zip_longest(*data, fillvalue = '')
+    if isIMPT:
+        csvFilePath = IMPTCSVPATH
+    else:
+        csvFilePath = ORIGINALCSVPATH
+    with open(csvFilePath + "leveldb_compaction_info_table_count.csv", 'w', encoding="ISO-8859-1", newline='') as myfile:
+        wr = csv.writer(myfile)
+        wr.writerow(["table count at each level"])
+        wr.writerow(columnTitles)
+        wr.writerows(export_data)
+    myfile.close()
+
+    data = [blockNums]
+    columnTitles = ["block number"]
+    for i in range(MAXLEVEL):
+        data.append(tableSizes[i][1:])
+        columnTitles.append("level " + str(i))
+    export_data = zip_longest(*data, fillvalue = '')
+    if isIMPT:
+        csvFilePath = IMPTCSVPATH
+    else:
+        csvFilePath = ORIGINALCSVPATH
+    with open(csvFilePath + "leveldb_compaction_info_table_size.csv", 'w', encoding="ISO-8859-1", newline='') as myfile:
+        wr = csv.writer(myfile)
+        wr.writerow(["table size at each level (MB)"])
+        wr.writerow(columnTitles)
+        wr.writerows(export_data)
+    myfile.close()
+
+    data = [blockNums]
+    columnTitles = ["block number"]
+    for i in range(MAXLEVEL):
+        data.append(durations[i][1:])
+        columnTitles.append("level " + str(i))
+    export_data = zip_longest(*data, fillvalue = '')
+    if isIMPT:
+        csvFilePath = IMPTCSVPATH
+    else:
+        csvFilePath = ORIGINALCSVPATH
+    with open(csvFilePath + "leveldb_compaction_info_duration.csv", 'w', encoding="ISO-8859-1", newline='') as myfile:
+        wr = csv.writer(myfile)
+        wr.writerow(["duration time at each level (sec)"])
+        wr.writerow(columnTitles)
+        wr.writerows(export_data)
+    myfile.close()
+
+    data = [blockNums]
+    columnTitles = ["block number"]
+    for i in range(MAXLEVEL):
+        data.append(readSizes[i][1:])
+        columnTitles.append("level " + str(i))
+    export_data = zip_longest(*data, fillvalue = '')
+    if isIMPT:
+        csvFilePath = IMPTCSVPATH
+    else:
+        csvFilePath = ORIGINALCSVPATH
+    with open(csvFilePath + "leveldb_compaction_info_read_size.csv", 'w', encoding="ISO-8859-1", newline='') as myfile:
+        wr = csv.writer(myfile)
+        wr.writerow(["read size at each level (MB)"])
+        wr.writerow(columnTitles)
+        wr.writerows(export_data)
+    myfile.close()
+
+    data = [blockNums]
+    columnTitles = ["block number"]
+    for i in range(MAXLEVEL):
+        data.append(writeSizes[i][1:])
+        columnTitles.append("level " + str(i))
+    export_data = zip_longest(*data, fillvalue = '')
+    if isIMPT:
+        csvFilePath = IMPTCSVPATH
+    else:
+        csvFilePath = ORIGINALCSVPATH
+    with open(csvFilePath + "leveldb_compaction_info_write_size.csv", 'w', encoding="ISO-8859-1", newline='') as myfile:
+        wr = csv.writer(myfile)
+        wr.writerow(["write size at each level (MB)"])
+        wr.writerow(columnTitles)
+        wr.writerows(export_data)
+    myfile.close()
+
+    #
+    # TODO?: drawing graphs
+    #
+
 
 
 if __name__ == "__main__":
@@ -298,6 +503,10 @@ if __name__ == "__main__":
     # analyze impt_data_log.txt log files
     analyzeDatabaseReadTimeLog(True)
     analyzeDatabaseReadTimeLog(False)
+
+    # analyze impt_leveldb_compaction_log.txt log files
+    analyzeLevelDBCompaction(True)
+    analyzeLevelDBCompaction(False)
 
     print("Done")
 
