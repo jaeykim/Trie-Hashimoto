94c5d4c16a1526218ccb091078d0c213929c8b59
jmlee <junmo.lee@snu.ac.kr>
2022-03-06 12:26:41 +0000

    comment out timestamp checking logic for experiment

--- miner/worker.go
+++ miner/worker.go
@@ -853,11 +853,11 @@ func (w *worker) commitNewWork(interrupt *int32, noempty bool, timestamp int64) 
 		timestamp = int64(parent.Time() + 1)
 	}
 	// this will ensure we're not going off too far in the future
-	if now := time.Now().Unix(); timestamp > now+1 {
-		wait := time.Duration(timestamp-now) * time.Second
-		log.Info("Mining too far in the future", "wait", common.PrettyDuration(wait))
-		//time.Sleep(wait) // remove sleeping term in mining (jmlee)
-	}
+	// if now := time.Now().Unix(); timestamp > now+1 {
+	// 	wait := time.Duration(timestamp-now) * time.Second
+	// 	log.Info("Mining too far in the future", "wait", common.PrettyDuration(wait))
+	// 	// time.Sleep(wait) // remove sleeping term in mining (jmlee)
+	// }
 
 	num := parent.Number()
 	header := &types.Header{
